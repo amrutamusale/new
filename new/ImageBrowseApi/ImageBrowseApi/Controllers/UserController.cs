f9e5f6c5442c4361fa04c3c8442b74a726d268bb
amrutamusale <musaleamruta1@gmail.com>
2022-07-07 09:25:55 +0530

    first commit

---
+++ new/ImageBrowseApi/ImageBrowseApi/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageBrowseApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        UserController db;
+        public UserController(UserController _db)
+        {
+            db = _db;
+        }
+    }
+}
---
+++ new/ImageBrowseApi/ImageBrowseApi/Models/TblUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ImageBrowseApi.Models
+{
+    public partial class TblUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
